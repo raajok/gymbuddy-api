a24ed86dbda464a229b7ee7b21e24a9a0250ecd8
Rasmus Jokinen <raajok@utu.fi>
2022-09-06 14:22:45 +0300

    MongoDB database connected to backend

--- api/app.js
+++ api/app.js
@@ -1,47 +1,30 @@
-var createError = require('http-errors');
+require('dotenv').config();
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cors = require("cors");
+var mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testAPI");
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+  console.log(error);
+});
+
+database.once('connected', () => {
+  console.log('Database Connected');
+});
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var testAPIRouter = require("./routes/testAPI");
 
-// test api
+// testing API connection
 app.use("/testAPI", testAPIRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(9000, () => {
+  console.log(`Server started at ${9000}`);
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
